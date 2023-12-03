53b5c8b464744f2b496c7bf12c460ac7f83c260f
tim <tim.leonhard@posteo.de>
2023-12-03 15:19:52 +0100

    solve day 3

---
+++ 03_gear_ratios/gear_ratios.py
@@ -0,0 +1,70 @@
+import time
+from itertools import product
+from math import prod
+
+begin = time.time()
+
+###
+
+NEIGHBOURHOOD = list(product([-1,0,1], repeat=2))
+
+def add_tuples(a ,b) -> tuple:
+	return tuple(map(sum, zip(a, b)))
+
+def get_neighbours(number: str, coord: tuple) -> set:
+	x, y = coord
+	result = []
+	for i in range(len(number)):
+		result += [add_tuples((x+i, y), n) for n in NEIGHBOURHOOD]
+	return set(result)
+
+def parse_numbers(lines: list) -> dict:
+	result = {}
+	for y, line in enumerate(lines):
+		buffer = ""
+		for x, char in enumerate(line.strip() + "."):
+			if char.isdigit():
+				buffer += char
+				continue
+			if buffer:
+				coord = x - len(buffer), y
+				result[coord] = buffer
+				buffer = ""
+	return result
+
+def parse_symbols(lines: list) -> dict:
+	result = {}
+	for y, line in enumerate(lines):
+		for x, char in enumerate(line.strip()):
+			if char == "." or char.isdigit():
+				continue
+			result[(x,y)] = char
+	return result
+
+
+with open("input.txt") as file:
+	inp = file.readlines()
+
+numbers = parse_numbers(inp)
+symbols = parse_symbols(inp)
+gear_symbols = {k: [] for k, v in symbols.items() if v == "*"}
+
+part_numbers = []
+for coordinates, n in numbers.items():
+	neighbours = get_neighbours(n, coordinates)
+	if symbols.keys() & neighbours:
+		part_numbers.append(int(n))
+	for g_coords in gear_symbols.keys() & neighbours:
+		gear_symbols[g_coords].append(int(n))
+
+gears = [numbers for _, numbers in gear_symbols.items() if len(numbers) == 2]
+gear_ratios = [prod(numbers) for numbers in gears]
+
+print(f"Part 1: {sum(n for n in part_numbers)}")
+print(f"Part 2: {sum(gear_ratios)}")
+
+###
+
+end = time.time()
+runtime_in_ms = round((end - begin)*1000, 3)
+print(f"Runtime: {runtime_in_ms:.3f} ms")
