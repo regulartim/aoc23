988f3fbcae0d6c51ba092cf2ccdd1b5b2b64d8ea
tim <tim.leonhard@posteo.de>
2023-12-16 21:23:18 +0100

    minor improvements

--- 16_the_floor_will_be_lava/the_floor_will_be_lava.py
+++ 16_the_floor_will_be_lava/the_floor_will_be_lava.py
@@ -5,39 +5,39 @@ begin = time.time()
 
 ###
 
-DIRECTIONS = [(-1,0), (0,-1), (1,0), (0,1)]
+DIRECTIONS = [(1,0), (0,1), (-1,0), (0,-1)]
 TURNS ={"/": [-1, 1, -1, 1], "\\": [1, -1, 1, -1]}
 
 def add_tuples(s: tuple, t: tuple) -> tuple:
 	return tuple(n + m for n, m in zip(s, t))
 
 @cache
-def get_next_states(x: int, y: int, d: int, tile: str) -> list:
-	if tile == "." or tile == "-|"[d%2]:
-		return [(*add_tuples((x,y), DIRECTIONS[d]), d)]
+def get_next_states(point: tuple, dir_idx: int, tile: str) -> list:
+	if tile == "." or tile == "-|"[dir_idx%2]:
+		return [(*add_tuples(point, DIRECTIONS[dir_idx]), dir_idx)]
 	if tile in "\\/":
-		turn = TURNS[tile][d]
-		d = (d + turn) % 4
-		return [(*add_tuples((x,y), DIRECTIONS[d]), d)]
+		turn = TURNS[tile][dir_idx]
+		dir_idx = (dir_idx + turn) % 4
+		return [(*add_tuples(point, DIRECTIONS[dir_idx]), dir_idx)]
 	if tile in "-|":
-		d1, d2 = (d + 1) % 4, (d - 1) % 4
-		return [(*add_tuples((x,y), DIRECTIONS[d1]), d1),
-				(*add_tuples((x,y), DIRECTIONS[d2]), d2)]
+		d1, d2 = (dir_idx + 1) % 4, (dir_idx - 1) % 4
+		return [(*add_tuples(point, DIRECTIONS[d1]), d1),
+				(*add_tuples(point, DIRECTIONS[d2]), d2)]
 	return []
 
-def energized_tiles(grid: dict, start: tuple) -> list:
+def energized_tiles(grid: dict, start: tuple) -> set:
 	seen = set()
-	q = [start]
-	while q:
-		current = q.pop()
-		x, y, d = current
+	stack = [start]
+	while stack:
+		current = stack.pop()
+		point, dir_idx = current[:2], current[-1]
 		if current in seen:
 			continue
-		if (x,y) not in grid:
+		if point not in grid:
 			continue
 		seen.add(current)
-		q += get_next_states(x, y, d, grid[(x,y)])
-	return list(set((x,y) for x,y,d in seen))
+		stack += get_next_states(point, dir_idx, grid[point])
+	return {(x,y) for x, y, dir_idx in seen}
 
 
 contraption_grid = {}
@@ -48,13 +48,13 @@ with open("input.txt") as file:
 			max_x = col
 			contraption_grid[(col, row)] = char
 
-starting_points = [(0, y, 2) for y in range(max_y)]
-starting_points += [(max_x, y, 0) for y in range(max_y)]
-starting_points += [(x, 0, 3) for x in range(max_x)]
-starting_points += [(x, max_y, 1) for x in range(max_x)]
+starting_points = [(0, y, 0) for y in range(max_y)] \
+ 				+ [(max_x, y, 2) for y in range(max_y)] \
+				+ [(x, 0, 1) for x in range(max_x)] \
+				+ [(x, max_y, 3) for x in range(max_x)]
 tile_counts = [len(energized_tiles(contraption_grid, s)) for s in starting_points]
 
-print(f"Part 1: {len(energized_tiles(contraption_grid, (0,0,2)))}")
+print(f"Part 1: {len(energized_tiles(contraption_grid, (0,0,0)))}")
 print(f"Part 2: {max(tile_counts)}")
 
 ###
