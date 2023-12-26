9469d4d7f38dd820a766d0d14992d4290f6131cc
tim <tim.leonhard@posteo.de>
2023-12-26 13:59:01 +0100

    improve performance by using self implemented dfs

--- 23_a_long_walk/a_long_walk.py
+++ 23_a_long_walk/a_long_walk.py
@@ -8,47 +8,40 @@ begin = time.time()
 def add_tuples(s: tuple, t: tuple) -> tuple:
 	return tuple(n + m for n, m in zip(s, t))
 
-def build_graph(grid, ignore_slopes):
+def build_graph(grid: dict, ignore_slopes: bool):
 	result = nx.DiGraph()
 	for p in grid:
-		neighbour_count = 0
 		for *delta, direction in [(0,1,"v"), (1,0,">"), (-1,0,"<"), (0,-1,"^")]:
 			n = add_tuples(p, delta)
 			if n not in grid:
 				continue
-			neighbour_count += 1
 			if grid[n] == ".":
-				result.add_edge(p, n)
+				result.add_edge(p, n, weight=1)
 			if ignore_slopes or grid[n] == direction:
-				result.add_edge(p, n)
+				result.add_edge(p, n, weight=1)
 	return result
 
-def find_junctions(graph) -> list:
-	return [node for node in graph.nodes() if len(list(graph.neighbors(node))) > 2]
+def edge_contraction(graph):
+	marked_for_removal = [node for node in graph.nodes() if len(graph[node]) == 2]
+	for node in marked_for_removal:
+		n1, n2 = list(graph[node])
+		new_weight = graph[node][n1]["weight"] + graph[node][n2]["weight"]
+		graph.add_edge(n1, n2, weight=new_weight)
+		graph.remove_node(node)
+	return graph
 
-def find_adjecent_junctions(graph, node: tuple, junctions: set) -> list:
-	result = []
-	for neighbor in graph.neighbors(node):
-		seen = {node, neighbor}
-		current = neighbor
-		while current not in junctions:
-			neighbors = [n for n in graph.neighbors(current) if n not in seen]
-			if len(neighbors) != 1:
-				print(node,current,neighbors )
-				return []
-			current = neighbors[0]
-			seen.add(current)
-		result.append((current, len(seen)))
-	return result
-
-def optimize(graph, junctions: set):
-	result = nx.MultiGraph()
-	for node in junctions:
-		for other, dist in find_adjecent_junctions(graph, node, junctions):
-			if result.has_edge(node, other) and result[node][other][0]["weight"] == dist:
-				continue
-			result.add_edge(node, other, weight=dist)
-	return result
+seen = set()
+def dfs_longest_path(node: tuple, target: tuple, neighbours: dict) -> int:
+	if node == target:
+		return 0
+	current_max = 0
+	seen.add(node)
+	for n, weight in neighbours[node]:
+		if n in seen:
+			continue
+		current_max = max(current_max, weight + dfs_longest_path(n, target, neighbours))
+	seen.remove(node)
+	return current_max
 
 
 hiking_map = {}
@@ -59,16 +52,17 @@ with open("input.txt") as file:
 				continue
 			hiking_map[(col, row)] = char
 
-start, target = min(hiking_map), max(hiking_map)
+top_row_tile, bottom_row_tile = min(hiking_map), max(hiking_map)
 p1_graph = build_graph(hiking_map, False)
-p2_graph = build_graph(hiking_map, True)
-trail_junctions = [start] + find_junctions(p2_graph) + [target]
-p2_graph = optimize(p2_graph, set(trail_junctions))
-p2_path_lenghts = [nx.path_weight(p2_graph, path, "weight") - len(path) + 1
-					for path in nx.all_simple_paths(p2_graph, start, target)]
+p2_graph = edge_contraction(build_graph(hiking_map, True).to_undirected())
 
-print(f"Part 1: {max([len(path)-1 for path in nx.all_simple_paths(p1_graph, start, target)])}")
-print(f"Part 2: {max(p2_path_lenghts)}")
+p2_neighbours = {node: [(n, p2_graph[node][n]["weight"]) for n in p2_graph[node]]
+						for node in p2_graph.nodes()}
+p1_neighbours = {node: [(n, weight) for n, weight in neighbours if nx.has_path(p1_graph, node, n)]
+						for node, neighbours in p2_neighbours.items()}
+
+print(f"Part 1: {dfs_longest_path(top_row_tile, bottom_row_tile, p1_neighbours)}")
+print(f"Part 2: {dfs_longest_path(top_row_tile, bottom_row_tile, p2_neighbours)}")
 
 ###
 
